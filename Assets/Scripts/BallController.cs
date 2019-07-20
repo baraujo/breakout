d8dee06f2d83e7f765bff948a5f4555be62c591e
Bruno Araújo <bruno@baraujo.net>
2019-07-19 22:33:44 -0400

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+// This script will control the ball behaviour
+// The ball will bounce on the sides and ceiling,
+// and the player will lose if the ball hits the bottom
+public class BallController : MonoBehaviour
+{
+    public float speed;
+    public float topScreenLimit;
+    public float leftScreenLimit;
+    public float rightScreenLimit;
+    public float bottomScreenLimit;
+
+    private Vector3 m_Direction;
+
+    void Start()
+    {
+        // Choose the initial ball direction
+        ChooseBallDirection();
+    }
+
+    private void ChooseBallDirection()
+    {
+        // There are four possible directions for the ball to fly,
+        // which are the four diagonals
+        
+        // Let's choose one using a random number
+        int randomDirection = Random.Range(0, 4);
+        
+        // This switch block will initialize m_Direction with
+        // a direction according to the randomDirection passed
+        // as parameter
+        switch(randomDirection)
+        {
+            case 0:
+                m_Direction.x = 1;
+                m_Direction.y = 1;
+                break;
+            case 1:
+                m_Direction.x = 1;
+                m_Direction.y = -1;
+                break;
+            case 2:
+                m_Direction.x = -1;
+                m_Direction.y = -1;
+                break;
+            case 3:
+                m_Direction.x = -1;
+                m_Direction.y = 1;
+                break;
+        }
+        transform.position = Vector3.zero;
+    }
+
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition += m_Direction * speed * Time.deltaTime;
+        transform.position = newPosition;
+
+        if(transform.position.y > topScreenLimit)
+        {
+            m_Direction.y *= -1;
+        }
+
+        if(transform.position.x < leftScreenLimit || transform.position.x > rightScreenLimit)
+        {
+            m_Direction.x *= -1;
+        }
+
+        if(transform.position.y < bottomScreenLimit)
+        {
+            ChooseBallDirection();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.name == "Player") 
+        {
+            m_Direction.y *= -1;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+// This script will read player actions and update the paddle state
+// The player can move the paddle left and right, inside the screen limits
+public class PlayerController : MonoBehaviour
+{
+    // Movement speed, in units/second
+    public float speed;
+    // How far the player can move to the left
+    public float leftScreenLimit;
+    // How far the player can move to the right
+    public float rightScreenLimit;
+
+    void Update()
+    {
+        // Verify if the left arrow is being pressed
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Store current position
+            Vector3 newPosition = transform.position;
+            
+            // Update X position
+            newPosition.x -= speed * Time.deltaTime;
+            
+            // Verify if the new position is beyond the limit
+            // If that's the case, limit it
+            if(newPosition.x < leftScreenLimit)
+            {
+                newPosition.x = leftScreenLimit;
+            }
+            
+            // Update the transform position
+            transform.position = newPosition;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            // Store current position
+            Vector3 newPosition = transform.position;
+            
+            // Update X position
+            newPosition.x += speed * Time.deltaTime;
+
+            // Verify if the new position is beyond the limit
+            // If that's the case, limit it
+            if(newPosition.x > rightScreenLimit)
+            {
+                newPosition.x = rightScreenLimit;
+            }
+            
+            // Update the transform position
+            transform.position = newPosition;
+        }
+    }
+}
